3b38b0091d9d0ea98e4770f80a4057a26dd10eca
Bartosz Jędrasik <arcybartek@gmail.com>
2018-07-08 03:41:12 +0200

    product attributes compression untangling in store

--- src/store/modules/products.js
+++ src/store/modules/products.js
@@ -1,16 +1,14 @@
 import { productsRef, attributesRef, attributesIdValueRef } from '../../firebase'
+import camelCase from 'lodash.camelcase'
 
-// initial state
 const state = {
     all: [],
     attributes: [],
     attributesIdValue: []
 }
 
-  // getters
 const getters = {}
 
-  // actions
 const actions = {
     async fetch ({ commit }) {
         let products = await productsRef.once('value')
@@ -24,12 +22,23 @@ const actions = {
     }
 }
 
-  // mutations
 const mutations = {
     setProducts (state, data) {
-        state.all = data.products
-        state.attributes = data.attributes
-        state.attributesIdValue = data.attributesIdValue
+        state.all = data.products.map(product => {
+            product.attributes = product.attributes.reduce((acc, attribute, idx) => {
+                let meta = data.attributes[idx]
+                if (meta.type === 'manyValue') {
+                    acc[camelCase(meta.name)] = data.attributesIdValue[idx].split('|').filter((val, idx) => {
+                        console.log(attribute.split('|'), idx)
+                        return attribute.split('|').includes(idx.toString())
+                    })
+                } else {
+                    acc[camelCase(meta.name)] = data.attributesIdValue[idx].split('|')[attribute]
+                }
+                return acc
+            }, {})
+            return product
+        })
     }
 }
 
